da4440bc5c8ef373bb627ee4cb4be08dc78eadd6
SenseiBaez <80366289+SenseiBaez@users.noreply.github.com>
2023-12-10 17:49:42 -0600

    Updated class wtih a header for class explanation.

--- src/snhu/jukebox/playlist/PlayableSong.java
+++ src/snhu/jukebox/playlist/PlayableSong.java
@@ -1,5 +1,36 @@
 package snhu.jukebox.playlist;
 
+/**
+ * PlayableSong.java
+ * 
+ * Author: Miguel Baez
+ * Contact: miguel.baez-flores@snhu.edu
+ * Date: November 15, 2023
+ * Version: 2.0
+ * 
+ * Purpose:
+ * This class is for creating playable song objects within the jukebox 
+ * playlist system. It encapsulates the common attributes of a song - its title and artist. This class 
+ * provides a base structure for specific types of songs that can be extended with additional
+ * functionalities as required.
+ * 
+ * Implementation Notes:
+ * - The class is designed to be abstract to allow for various types of playable songs with different
+ *   characteristics.
+ * - The title and artist properties are set through a protected constructor, making them immutable 
+ *   after initialization.
+ * - Getter methods are provided for accessing the title and artist of a song.
+ * 
+ * Usage:
+ * This class should be extended by other classes that represent specific types of songs in a music 
+ * management system.
+ * 
+ * 
+ * Revision History:
+ * - 2.0: Updated release. Basic structure for playable songs with title and artist attributes.
+ */
+
+
 public abstract class PlayableSong {
     private String title;
     private String artist;
@@ -17,7 +48,5 @@ public abstract class PlayableSong {
     public String getArtist() {
         return artist;
     }
-    
-    // If the play method is needed for future use or other parts of the application, uncomment it.
-    // abstract void play();
 }
+
